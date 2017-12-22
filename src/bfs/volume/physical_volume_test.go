fdcfb3c04260a7494b1f4b34b4c7c74b3f773e9b
Eric Sammer <esammer@gmail.com>
2017-12-22 14:01:39 -0800

    Added basic physical volume (block) reader/writer tests.

--- src/bfs/volume/physical_volume_test.go
+++ src/bfs/volume/physical_volume_test.go
@@ -98,3 +98,49 @@ func TestPhysicalVolume_StateTransitions(t *testing.T) {
 		}
 	})
 }
+
+func TestPhysicalVolume_ReaderWriter(t *testing.T) {
+	pv := NewPhysicalVolume("build/test/" + t.Name())
+
+	if err := pv.Open(true); err != nil {
+		t.Error("Open failed for non-existant path - %v", err)
+	}
+
+	if writer, err := pv.WriterFor("1"); err == nil {
+		if _, err := io.WriteString(writer, "Test 1"); err != nil {
+			t.Errorf("Faled to write to new block - %v", err)
+		}
+
+		if err := writer.Close(); err != nil {
+			t.Errorf("Failed to close writer - %v", err)
+		}
+	} else {
+		t.Errorf("Failed to create writer - %v", err)
+	}
+
+	if reader, err := pv.ReaderFor("1"); err == nil {
+		buffer := make([]byte, 16)
+
+		if size, err := reader.Read(buffer); err == nil {
+			if string(buffer[:size]) != "Test 1" {
+				t.Errorf("Buffer not as expected: %v", string(buffer[:size]))
+			}
+		} else {
+			t.Errorf("Failed to read from new block - %v", err)
+		}
+
+		if err := reader.Close(); err != nil {
+			t.Errorf("Failed to close reader - %v", err)
+		}
+	} else {
+		t.Errorf("Failed to create reader - %v", err)
+	}
+
+	if err := pv.Close(); err != nil {
+		t.Fatalf("Failed to close volume - %v", err)
+	}
+
+	if err := os.RemoveAll("build/test/" + t.Name()); err != nil {
+		t.Fatalf("Failed to remove test directory - %v", err)
+	}
+}
