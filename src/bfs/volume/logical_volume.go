c0dcf78a38759a932d6a57c6a0502eeba1f585db
Eric Sammer <esammer@gmail.com>
2017-12-22 16:27:24 -0800

    Created a LogicalVolume prototype that adds a file layer.
    * Logical volumes aggregate and build on top of physical volumes. Specifically,
      a logical volume has an array of physical volumes. This prototype only supports
      a single pv in that array.
    * No support for file metadata or block mapping storage exists. All metadata is
      currently just discarded by the writer.
    * No reader implementation exists yet. This is really just a prototype of the block
      striping in the writer to prove that it works.
    * The tests currently write (and log) a lot; about 10MB per run. That will change.

---
+++ src/bfs/volume/logical_volume.go
@@ -0,0 +1,160 @@
+package volume
+
+import (
+	"fmt"
+	"log"
+
+	"bfs/block"
+)
+
+/*
+ * Writer
+ */
+
+type Writer struct {
+	lv        *LogicalVolume
+	blockSize int
+	filename  string
+
+	filePos int
+
+	blockWriter block.BlockWriter
+	blockPos    int
+	blockCount  int
+}
+
+func NewFileWriter(lv *LogicalVolume, filename string, blockSize int) *Writer {
+	log.Printf("Allocate writer for %v with blockSize %d on %#v", filename, blockSize, lv)
+
+	return &Writer{
+		lv:        lv,
+		blockSize: blockSize,
+		filename:  filename,
+	}
+}
+
+func (this *Writer) Write(buffer []byte) (int, error) {
+	bufferPos := 0
+	bufferRemaining := len(buffer)
+
+	for bufferRemaining > 0 {
+		writeLen := 0
+
+		if this.blockPos == this.blockSize || this.blockCount == 0 {
+			this.blockCount++
+
+			log.Printf("Allocated block %d - old blockPos: %d filePos: %d", this.blockCount, this.blockPos, this.filePos)
+
+			if this.blockWriter != nil {
+				if err := this.blockWriter.Close(); err != nil {
+					return 0, err
+				}
+			}
+
+			if blockWriter, err := this.lv.volumes[0].WriterFor(fmt.Sprintf("%d", this.blockCount)); err == nil {
+				this.blockWriter = blockWriter
+			} else {
+				return 0, err
+			}
+
+			this.blockPos = 0
+		}
+
+		if bufferRemaining < (this.blockSize - this.blockPos) {
+			writeLen = bufferRemaining
+		} else {
+			writeLen = (this.blockSize - this.blockPos)
+		}
+
+		if writeLen > 0 {
+			log.Printf("Write %d:%d of %d bytes to %v", bufferPos, bufferPos+writeLen, len(buffer), this.filename)
+
+			if _, err := this.blockWriter.Write(buffer[bufferPos : bufferPos+writeLen]); err != nil {
+				return 0, err
+			}
+
+			this.filePos += writeLen
+			this.blockPos += writeLen
+			bufferPos += writeLen
+			bufferRemaining -= writeLen
+
+			//log.Printf("State bufferPos: %d bufferRemaining: %d blockPos: %d", bufferPos, bufferRemaining, this.blockPos)
+		}
+	}
+
+	return 0, nil
+}
+
+func (this *Writer) Close() error {
+	log.Printf("Closing writer for file %v on %#v", this.filename, this.lv)
+
+	if this.blockWriter != nil {
+		return this.blockWriter.Close()
+	}
+
+	return nil
+}
+
+/*
+ * LogicalVolume
+ */
+
+type LogicalVolume struct {
+	Namespace string
+	volumes   []*PhysicalVolume
+	state     VolumeState
+}
+
+func NewLogicalVolume(namespace string, volumes []*PhysicalVolume) *LogicalVolume {
+	log.Printf("Allocate logical volume for namespace %v", namespace)
+
+	return &LogicalVolume{
+		Namespace: namespace,
+		volumes:   volumes,
+		state:     VOLUME_INITIAL,
+	}
+}
+
+func (this *LogicalVolume) Open() error {
+	log.Printf("Open logical volume for namespace %v", this.Namespace)
+
+	if this.state != VOLUME_INITIAL {
+		return fmt.Errorf("Attempt to open volume from state %v", this.state)
+	}
+
+	this.state = VOLUME_OPEN
+
+	return nil
+}
+
+func (this *LogicalVolume) Close() error {
+	log.Printf("Close logical volume for namespace %v", this.Namespace)
+
+	if this.state != VOLUME_OPEN {
+		return fmt.Errorf("Attempt to close volume from state %v", this.state)
+	}
+
+	this.state = VOLUME_CLOSED
+
+	return nil
+}
+
+func (this *LogicalVolume) WriterFor(filename string, blockSize int) (*Writer, error) {
+	if this.state != VOLUME_OPEN {
+		return nil, fmt.Errorf("Attempt to open writer from volume in state %v", this.state)
+	}
+
+	log.Printf("Opening writer for %v with block size %v", filename, blockSize)
+
+	return NewFileWriter(this, filename, blockSize), nil
+}
+
+func (this *LogicalVolume) ReaderFor(filename string) error {
+	if this.state != VOLUME_OPEN {
+		return fmt.Errorf("Attempt to open reader from volume in state %v", this.state)
+	}
+
+	log.Printf("Opening reader for %v", filename)
+
+	return nil
+}
---
+++ src/bfs/volume/logical_volume_test.go
@@ -0,0 +1,70 @@
+package volume
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestLogicalVolume_OpenClose(t *testing.T) {
+	lv := NewLogicalVolume("/", nil)
+
+	if err := lv.Open(); err != nil {
+		t.Fatalf("Failed to open volume - %v", err)
+	}
+
+	if err := lv.Close(); err != nil {
+		t.Fatalf("Failed to close volume - %v", err)
+	}
+}
+
+func TestLogicalVolume_ReaderWriter(t *testing.T) {
+	if err := os.RemoveAll("build/test/" + t.Name()); err != nil {
+		t.Fatalf("Failed to remove test directory - %v", err)
+	}
+
+	pv := NewPhysicalVolume("build/test/" + t.Name())
+
+	if err := pv.Open(true); err != nil {
+		t.Fatalf("Failed to open physical volume - %v", err)
+	}
+
+	lv := NewLogicalVolume("/", []*PhysicalVolume{pv})
+
+	if err := lv.Open(); err != nil {
+		t.Fatalf("Failed to open volume - %v", err)
+	}
+
+	if err := os.MkdirAll("build/test/"+t.Name(), 0700); err != nil {
+		t.Fatalf("Failed to create test directory - %v", err)
+	}
+
+	if writer, err := lv.WriterFor("test1.txt", 1024*1024); err == nil {
+		for i := 0; i < 1000000; i++ {
+			if _, err := io.WriteString(writer, fmt.Sprintf("Test %d\n", i)); err != nil {
+				t.Fatalf("Failed to write to writer - %v", err)
+			}
+		}
+
+		if err := writer.Close(); err != nil {
+			t.Fatalf("Failed to close writer - %v", err)
+		}
+	} else {
+		t.Fatalf("Unable to open writer - %v", err)
+	}
+
+	if err := lv.Close(); err != nil {
+		t.Fatalf("Failed to close volume - %v", err)
+	}
+
+	if err := pv.Close(); err != nil {
+		t.Fatalf("Failed to close physical volume - %v", err)
+	}
+
+	/*
+		if err := os.RemoveAll("build/test/" + t.Name()); err != nil {
+			t.Fatalf("Failed to remove test directory - %v", err)
+		}
+	*/
+}
