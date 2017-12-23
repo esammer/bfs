027735a86d70ed419d858f60a33e233a71d80a73
Eric Sammer <esammer@gmail.com>
2017-12-22 21:18:13 -0800

    Use an lv to perform the writes in main().

--- src/bfs/bfs.go
+++ src/bfs/bfs.go
@@ -1,46 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"github.com/golang/glog"
 	"io"
-	"log"
 
 	"bfs/volume"
 )
 
 func main() {
+	flag.Parse()
+
 	pv := volume.NewPhysicalVolume("tmp/1")
 
 	if err := pv.Open(true); err != nil {
-		log.Fatalf("Unable to open volume - %v", err)
+		glog.Fatalf("Unable to open volume - %v", err)
 	}
 
+	lv := volume.NewLogicalVolume("/", []*volume.PhysicalVolume{pv})
+
+	lv.Open()
+
 	for i := 1; i <= 3; i++ {
-		if writer, err := pv.WriterFor(fmt.Sprintf("%d", i)); err != nil {
-			log.Fatalf("Unable to open block for write - %v", err)
+		if writer, err := lv.WriterFor("/hello.txt", 8); err != nil {
+			glog.Fatalf("Unable to open writer - %v", err)
 		} else {
-			io.WriteString(writer, fmt.Sprintf("Hello %v", i))
-			writer.Close()
-
-			buffer := make([]byte, 16)
-
-			if reader, err := pv.ReaderFor(fmt.Sprintf("%d", i)); err == nil {
-				if size, err := reader.Read(buffer); err == nil {
-					log.Printf("Read %v bytes", size)
-
-					if fmt.Sprintf("Hello %v", i) != string(buffer[:size]) {
-						log.Fatalf("Strings are not equals")
-					}
-				} else {
-					log.Fatalf("Unable to read from block - %v", err)
-				}
-
-				reader.Close()
-			}
+			io.WriteString(writer, "Hello world. How do you do?")
 		}
 	}
 
+	lv.Close()
+
 	if err := pv.Close(); err != nil {
-		log.Fatalf("Unable to close volume - %v", err)
+		glog.Fatalf("Unable to close volume - %v", err)
 	}
+
+	glog.Flush()
 }
