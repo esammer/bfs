5f9c1dcaf61a2ece3878781bf1fec8156d6fe8fa
Eric Sammer <esammer@gmail.com>
2017-12-30 18:02:30 -0800

    Reworked bfs demo to properly configure the system with flags.

--- src/bfs/bfs.go
+++ src/bfs/bfs.go
@@ -1,42 +1,126 @@
 package main
 
 import (
+	"bfs/block"
+	"bfs/ns"
+	"bytes"
 	"flag"
+	"fmt"
 	"github.com/golang/glog"
-	"io"
+	"os"
+	"strings"
 
 	"bfs/volume"
 )
 
+type Config struct {
+	NamespacePath       string
+	VolumePaths         ListValue
+	AllowInitialization bool
+}
+
+type ListValue []string
+
+func (this *ListValue) Set(value string) error {
+	*this = append(*this, value)
+	return nil
+}
+
+func (this *ListValue) String() string {
+	return strings.Join(*this, ",")
+}
+
 func main() {
+	config := &Config{}
+
+	flag.StringVar(&config.NamespacePath, "ns", "", "namespace directory")
+	flag.BoolVar(&config.AllowInitialization, "autoinit", false, "allow auto-initialization of physical volumes")
+	flag.Var(&config.VolumePaths, "volume", "physical volume directory (repeatable)")
+
 	flag.Parse()
 
-	eventsChannel := make(chan interface{}, 1024)
-
-	pv := volume.NewPhysicalVolume("tmp/1", eventsChannel)
-
-	if err := pv.Open(true); err != nil {
-		glog.Fatalf("Unable to open volume - %v", err)
+	if len(config.NamespacePath) == 0 {
+		glog.Error("--ns is required")
+		os.Exit(1)
+	}
+	if len(config.VolumePaths) == 0 {
+		glog.Error("At least one --volume is required")
+		os.Exit(1)
 	}
 
-	lv := volume.NewLogicalVolume("/", []*volume.PhysicalVolume{pv}, eventsChannel)
+	pvs := make([]*volume.PhysicalVolume, 0, len(config.VolumePaths))
+	eventChannel := make(chan interface{}, 1024)
 
-	lv.Open()
+	namespace := ns.New(config.NamespacePath)
+	if err := namespace.Open(); err != nil {
+		glog.Errorf("Failed to open namespace %s - %s", config.NamespacePath, err)
+		os.Exit(1)
+	}
+	if err := namespace.Close(); err != nil {
+		glog.Errorf("Failed to close namespace %s - %s", config.NamespacePath, err)
+		os.Exit(1)
+	}
 
-	for i := 1; i <= 3; i++ {
-		if writer, err := lv.WriterFor(nil, "/hello.txt", 8); err != nil {
-			glog.Fatalf("Unable to open writer - %v", err)
-		} else {
-			io.WriteString(writer, "Hello world. How do you do?")
-			writer.Close()
+	go func() {
+		for event := range eventChannel {
+			switch e := event.(type) {
+			case *block.BlockWriteEvent:
+				e.AckChannel <- e
+			}
+		}
+	}()
+
+	for _, path := range config.VolumePaths {
+		pv := volume.NewPhysicalVolume(path, eventChannel)
+		pvs = append(pvs, pv)
+	}
+
+	for _, pv := range pvs {
+		err := pv.Open(config.AllowInitialization)
+		if err != nil {
+			glog.Errorf("Failed to open volume %s - %s", pv.RootPath, err)
+			os.Exit(1)
 		}
 	}
 
-	lv.Close()
-
-	if err := pv.Close(); err != nil {
-		glog.Fatalf("Unable to close volume - %v", err)
+	fs := &volume.LocalFileSystem{
+		Namespace:    namespace,
+		Volumes:      []*volume.LogicalVolume{volume.NewLogicalVolume("/", pvs, eventChannel)},
+		EventChannel: eventChannel,
 	}
+
+	if err := fs.Open(); err != nil {
+		glog.Errorf("Unable to open filesystem - %s", err)
+		os.Exit(1)
+	}
+
+	zeroBuf := bytes.Repeat([]byte{0}, 1024*1024)
+
+	for i := 1; i <= 10; i++ {
+		if writer, err := fs.OpenWrite(fmt.Sprintf("/tmp/%d", i), 256*1024); err != nil {
+			glog.Fatalf("Unable to open writer - %v", err)
+		} else {
+			if _, err := writer.Write(zeroBuf); err != nil {
+				glog.Errorf("Unable to write to file - %v", err)
+			}
+
+			if err := writer.Close(); err != nil {
+				glog.Errorf("Unable to close writer for file - %v", err)
+			}
+		}
+	}
+
+	fs.Close()
+
+	for _, pv := range pvs {
+		err := pv.Close()
+		if err != nil {
+			glog.Errorf("Failed to close volume %s - %s", pv.RootPath, err)
+			os.Exit(1)
+		}
+	}
+
+	close(eventChannel)
 
 	glog.Flush()
 }
