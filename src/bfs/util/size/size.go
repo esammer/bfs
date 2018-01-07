945637205ad38d17e26a8b4bbe7381d2de2e710d
Eric Sammer <esammer@gmail.com>
2018-01-06 22:45:05 -0800

    Added a size datatype for expressing SI byte units.

---
+++ src/bfs/util/size/size.go
@@ -0,0 +1,79 @@
+package size
+
+import "fmt"
+
+type Size float64
+
+const (
+	B  = 1
+	KB = 1024
+	MB = KB * 1024
+	GB = MB * 1024
+	TB = GB * 1024
+	PB = TB * 1024
+)
+
+func Bytes(size float64) Size {
+	return Size(size)
+}
+
+func Kilobytes(size float64) Size {
+	return Size(size * KB)
+}
+
+func Megabytes(size float64) Size {
+	return Size(size * MB)
+}
+
+func Gigabytes(size float64) Size {
+	return Size(size * GB)
+}
+
+func Terabytes(size float64) Size {
+	return Size(size * TB)
+}
+
+func Petabytes(size float64) Size {
+	return Size(size * PB)
+}
+
+func (this *Size) ToBytes() float64 {
+	return float64(*this)
+}
+
+func (this *Size) ToKilobytes() float64 {
+	return float64(*this) / KB
+}
+
+func (this *Size) ToMegabytes() float64 {
+	return float64(*this) / MB
+}
+
+func (this *Size) ToGigabytes() float64 {
+	return float64(*this) / GB
+}
+
+func (this *Size) ToTerabytes() float64 {
+	return float64(*this) / TB
+}
+
+func (this *Size) ToPetabytes() float64 {
+	return float64(*this) / PB
+}
+
+func (this *Size) String() string {
+	switch {
+	case float64(*this) >= PB:
+		return fmt.Sprint(this.ToPetabytes()) + "PB"
+	case float64(*this) >= TB:
+		return fmt.Sprint(this.ToTerabytes()) + "TB"
+	case float64(*this) >= GB:
+		return fmt.Sprint(this.ToGigabytes()) + "GB"
+	case float64(*this) >= MB:
+		return fmt.Sprint(this.ToMegabytes()) + "MB"
+	case float64(*this) >= KB:
+		return fmt.Sprint(this.ToKilobytes()) + "KB"
+	default:
+		return fmt.Sprint(*this) + "B"
+	}
+}
---
+++ src/bfs/util/size/size_test.go
@@ -0,0 +1,46 @@
+package size
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSize_Bytes(t *testing.T) {
+	size := Bytes(1024 * 1024 * 1024 * 1024 * 1024)
+	require.Equal(t, float64(1024*1024*1024*1024), size.ToKilobytes())
+	require.Equal(t, float64(1024*1024*1024), size.ToMegabytes())
+	require.Equal(t, float64(1024*1024), size.ToGigabytes())
+	require.Equal(t, float64(1024), size.ToTerabytes())
+	require.Equal(t, float64(1), size.ToPetabytes())
+	require.Equal(t, "1PB", size.String())
+}
+
+func TestSize_Kilobytes(t *testing.T) {
+	size := Bytes(KB + KB/2)
+	require.Equal(t, 1.5, size.ToKilobytes())
+	require.Equal(t, "1.5KB", size.String())
+}
+
+func TestSize_Megabytes(t *testing.T) {
+	size := Bytes(MB + MB/2)
+	require.Equal(t, 1.5, size.ToMegabytes())
+	require.Equal(t, "1.5MB", size.String())
+}
+
+func TestSize_Gigabytes(t *testing.T) {
+	size := Bytes(GB + GB/2)
+	require.Equal(t, 1.5, size.ToGigabytes())
+	require.Equal(t, "1.5GB", size.String())
+}
+
+func TestSize_Terabytes(t *testing.T) {
+	size := Bytes(TB + TB/2)
+	require.Equal(t, 1.5, size.ToTerabytes())
+	require.Equal(t, "1.5TB", size.String())
+}
+
+func TestSize_Petabytes(t *testing.T) {
+	size := Bytes(PB + PB/2)
+	require.Equal(t, 1.5, size.ToPetabytes())
+	require.Equal(t, "1.5PB", size.String())
+}
