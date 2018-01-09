213c092a30580d5826bbd9c505e8e056cc7787c9
Eric Sammer <esammer@gmail.com>
2018-01-08 19:35:50 -0800

    Reimplemented bfs prototype to run either a server or client.
    Major new prototype. New features:
    
    * `bfs client` acts as a CLI for the system. It supports:
    ** --server <host:port> - The server to connect to.
    ** --stat <path> - File stat operations.
    ** --put <path> - File upload/put operations with --block-size <bytes>.
    ** --get <path> - File download/get operations.
    * `bfs server` acts as a combined name+block server. It supports:
    ** --ns <path> - Name system storage path.
    ** --volume <path> - A physical volume storage path. Repeatable.
    ** --auto-init / -a - Support for auto-initialization of name and block paths.
    ** --bind <host:port> - The host and port to bind to.
    * Both run modes support the awful but standard glog options and -h.
    * Known issues:
    ** --put and --get don't allow the specification of a destination path. That is,
       `--put /tmp/foo.txt` copies the local file to exact same path in bfs. Get
       has the same limitation. To prevent accidental overwrite of local files, get
       currently _always_ appends `.new` to the name of files.
    ** Lots of missing error checking. Things may crash and burn.
    ** There's no global unified --help.

--- src/bfs/bfs.go
+++ src/bfs/bfs.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"bfs/blockservice"
+	"bfs/nameservice"
+	"bfs/ns"
+	"bfs/volume"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/golang/glog"
+	"google.golang.org/grpc"
+	"io"
+	"net"
+	"os"
+	"os/signal"
 	"strings"
-
+	"syscall"
 )
 
 type Config struct {
 	NamespacePath       string
 	VolumePaths         ListValue
 	AllowInitialization bool
+	BindAddress         string
+
+	PutFile   string
+	GetFile   string
+	StatPath  string
+	BlockSize int
 }
 
 type ListValue []string
@@ -23,7 +42,178 @@ func (this *ListValue) String() string {
 }
 
 func main() {
+	config := &Config{}
 
+	clientFlags := flag.NewFlagSet("client", flag.ContinueOnError)
+	clientFlags.StringVar(&config.BindAddress, "server", "", "server host:port")
+	clientFlags.IntVar(&config.BlockSize, "block-size", 0, "block size for write (in MB)")
+	clientFlags.StringVar(&config.PutFile, "put", "", "put file")
+	clientFlags.StringVar(&config.GetFile, "get", "", "get file")
+	clientFlags.StringVar(&config.StatPath, "stat", "", "stat a file")
+	flag.CommandLine.VisitAll(func(f *flag.Flag) {
+		clientFlags.Var(f.Value, f.Name, f.Usage)
+	})
+
+	serverFlags := flag.NewFlagSet("server", flag.ContinueOnError)
+	serverFlags.StringVar(&config.NamespacePath, "ns", "", "namespace directory")
+	serverFlags.StringVar(&config.BindAddress, "bind", "127.0.0.1:60000", "bind address")
+	serverFlags.BoolVar(&config.AllowInitialization, "auto-init", false, "allow auto-initialization of physical volumes")
+	serverFlags.BoolVar(&config.AllowInitialization, "a", false, "allow auto-initialization of physical volumes")
+	serverFlags.Var(&config.VolumePaths, "volume", "physical volume directory (repeatable)")
+	flag.CommandLine.VisitAll(func(f *flag.Flag) {
+		serverFlags.Var(f.Value, f.Name, f.Usage)
+	})
+
+	flag.Parse()
+
+	args := os.Args
+	if len(args) > 2 {
+		switch args[1] {
+		case "client":
+			clientFlags.Parse(args[2:])
+			runClient(config)
+		case "server":
+			serverFlags.Parse(args[2:])
+			runServer(config)
+		}
+	}
+}
+
+func runClient(config *Config) {
+	conn, err := grpc.Dial(config.BindAddress, grpc.WithInsecure())
+	if err != nil {
+		glog.Errorf("Failed to connect to server %s - %v", config.BindAddress, err)
+	}
+	defer conn.Close()
+
+	nameClient := nameservice.NewNameServiceClient(conn)
+	blockClient := blockservice.NewBlockServiceClient(conn)
+
+	if len(config.PutFile) != 0 {
+		reader, err := os.Open(config.PutFile)
+		if err != nil {
+			glog.Errorf("Unable to open %s - %v", config.PutFile, err)
+			return
+		}
+		defer reader.Close()
+
+		pvIds, err := blockClient.Volumes(context.Background(), &blockservice.VolumeRequest{})
+		if err != nil {
+			glog.Errorf("Unable to get volume metadata - %v", err)
+			return
+		}
+
+		writer := volume.NewWriter(nameClient, blockClient, pvIds.VolumeId, config.PutFile, config.BlockSize, nil)
+		defer writer.Close()
+
+		written, err := io.Copy(writer, reader)
+		if err != nil {
+			glog.Errorf("Failed to copy %s - %v", config.PutFile, err)
+			return
+		}
+
+		glog.Infof("Copied %d bytes", written)
+	} else if len(config.GetFile) != 0 {
+		reader := volume.NewReader(nameClient, blockClient, config.GetFile)
+		if err := reader.Open(); err != nil {
+			glog.Errorf("Failed to open file reader - %v", err)
+			return
+		}
+		defer reader.Close()
+
+		writer, err := os.Create(config.GetFile + ".new")
+		if err != nil {
+			glog.Errorf("Unable to open %s.new for write - %v", config.GetFile, err)
+			return
+		}
+		defer writer.Close()
+
+		written, err := io.Copy(writer, reader)
+		if err != nil {
+			glog.Errorf("Unable to copy file - %v", err)
+			return
+		}
+
+		glog.Infof("Copied %d bytes", written)
+	} else if len(config.StatPath) != 0 {
+		resp, err := nameClient.Get(context.Background(), &nameservice.GetRequest{Path: config.StatPath})
+		if err != nil {
+			glog.Errorf("Stat failed - %v", err)
+			return
+		}
+
+		fmt.Printf("%s (%d blocks)\n", resp.Entry.Path, len(resp.Entry.Blocks))
+
+		for i, block := range resp.Entry.Blocks {
+			fmt.Printf("  %3d: %s (%s)\n", i, block.BlockId, block.PvId)
+		}
+	}
+}
+
+func runServer(config *Config) {
+	if len(config.NamespacePath) == 0 {
+		glog.Error("--ns is required")
+		os.Exit(1)
+	}
+	if len(config.VolumePaths) == 0 {
+		glog.Error("At least one --volume is required")
+		os.Exit(1)
+	}
+
+	pvs := make([]*blockservice.PhysicalVolume, len(config.VolumePaths))
+	for i, path := range config.VolumePaths {
+		pvs[i] = blockservice.NewPhysicalVolume(path)
+		pvs[i].Open(config.AllowInitialization)
+	}
+
+	namespace := ns.New(config.NamespacePath)
+	namespace.Open()
+
+	nameService := nameservice.New(namespace)
+	blockService := blockservice.New(pvs)
+
+	listener, err := net.Listen("tcp", config.BindAddress)
+	if err != nil {
+		glog.Errorf("Failed to bind to %s - %v", config.BindAddress, err)
+		os.Exit(1)
+	}
+
+	server := grpc.NewServer()
+	blockservice.RegisterBlockServiceServer(server, blockService)
+	nameservice.RegisterNameServiceServer(server, nameService)
+
+	signalChan := make(chan os.Signal, 8)
+	go func() {
+		for sig := range signalChan {
+			switch sig {
+			case os.Interrupt, syscall.SIGTERM:
+				glog.Info("Shutting down")
+				server.GracefulStop()
+			default:
+			}
+		}
+	}()
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	rpcQuitChan := make(chan error)
+
+	go func() {
+		err := server.Serve(listener)
+		rpcQuitChan <- err
+	}()
+
+	glog.Infof("Server running on %s - use SIGINT to stop", config.BindAddress)
+
+	err = <-rpcQuitChan
+	if err != nil {
+		glog.Errorf("RPC server failed - %v", err)
+	}
+
+	namespace.Close()
+
+	for _, pv := range pvs {
+		pv.Close()
+	}
 }
 
 /*
